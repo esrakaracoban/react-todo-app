51c29e8c2870029cd42b2208b6e419b20930af3a
esrakaracoban <pakesra@hotmail.com>
2022-09-08 17:36:19 +0300

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import React from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+     
+      
+    </div>
+  );
+}
+
+export default App;
